8b5ae42d6f933def23450e259892b71f9a166709
hao yue <haoyue@publicissapient.com>
2024-05-23 16:03:49 +0800

    :change: change docs imports

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 import DefaultTheme from "vitepress/theme";
 import { type App } from "vue";
 import HaosamaUI from "../../../core/index";
-import 'haosama-ui/dist/index.css'
+// import 'haosama-ui/dist/index.css'
 import { ElementPlusContainer } from "@vitepress-demo-preview/component";
 
 import "@vitepress-demo-preview/component/dist/style.css";
